a60bcd397ee7e94f2eb8042863e2879fdbf28164
DominguezSergio <49977576+DominguezSergio@users.noreply.github.com>
2019-11-05 10:55:07 +0100

    Add files via upload

---
+++ 01_Web_MySQL/src/org/datos/AccesoBasico.java
@@ -0,0 +1,41 @@
+package org.datos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AccesoBasico {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			//Primer paso
+			Class.forName("com.mysql.jdbc.Driver"); //Busca la clase Driver en las librerias
+			System.out.println("Conector OK!");
+			//Segundo paso
+			String url = "jdbc:mysql://localhost:3306/primera";
+			String user = "root";
+			String password = "";
+			Connection conn = DriverManager.getConnection(url, user, password);
+			System.out.println("La conexion es: " + conn.toString());
+			//Tercer paso
+			String consulta = "select * from clientes";
+			Statement st = conn.createStatement();
+			ResultSet resultado = st.executeQuery(consulta);
+			System.out.println("El ResultSet es: " + resultado);
+			//Cuarto paso
+			while(resultado.next()) {
+				System.out.println(resultado.getString("nombre"));
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
